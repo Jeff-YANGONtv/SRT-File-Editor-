1a4129500a83dbce1d3f95879f522df1e446f74b
Jeff <jeff372001@gmail.com>
2026-04-19 20:45:59 +0630

    Update layout .tsx

--- app/layout .tsx
+++ app/layout .tsx
@@ -8,7 +8,7 @@ export const metadata = {
   description: 'Internal Subtitle Management System',
 }
 
-export default function RootLayout({ children }: { children: .ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
